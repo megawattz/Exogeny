1f6fc4be9c4ae0402dd1774394d2d5700699a3b4
Walter Howard <walterhoward@gmail.com>
2024-06-01 21:31:18 -0400

    New contract compiles and deploys and intializes. Not sure how well it works

--- planetor/tools/seq.sh
+++ planetor/tools/seq.sh
@@ -1,11 +1,12 @@
 #!/bin/bash
 
-COUNT=$1
-echo "[$(seq -s, 0 ${COUNT})]";
+START=$1
+COUNT=$2
+echo "[$(seq -s, ${START} ${COUNT})]";
 
 MINUS=$(($COUNT-1))
 echo -n "[";
-for N in $(seq 0 ${MINUS} ); do echo -n "1," ; done;
+for N in $(seq ${START} ${MINUS} ); do echo -n "1," ; done;
 echo "1]";
 
 
