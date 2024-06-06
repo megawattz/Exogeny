455c4ad5cb8d07d29c656ae8e90c98aae176ce55
Walter Howard <walterhoward@gmail.com>
2024-06-06 00:21:26 -0400

    before balanceOfBatch

--- website/js/web3.mjs
+++ website/js/web3.mjs
@@ -1,19 +1,56 @@
 // Check if MetaMask is installed
+
+const EVMchains = {
+    arbitrumOne: "0xa4b1",         // Arbitrum One
+    arbitrumRinkeby: "0x66eeb",    // Arbitrum Rinkeby Testnet
+    avalanche: "0xa86a",           // Avalanche C-Chain
+    AVAX: "0xa86a",           	   // Avalanche C-Chain
+    avalancheFuji: "0xa869",       // Avalanche Fuji C-Chain (Testnet)
+    binanceSmartChain: "0x38",     // Binance Smart Chain Mainnet
+    BNB: "0x38",     // Binance Smart Chain Mainnet
+    bscTestnet: "0x61",            // Binance Smart Chain Testnet
+    celo: "0xa4ec",                // Celo Mainnet
+    celoAlfajores: "0xaef3",       // Celo Alfajores Testnet
+    cronos: "0x19",                // Cronos Mainnet
+    cronosTestnet: "0x152",        // Cronos Testnet
+    ethereum: "0x1",               // Ethereum Mainnet
+    ETH: "0x1",                    // Ethereum Mainnet
+    sepolia: "0xaa36a7",	   // Sepolia (ethereum Testnet)
+    fantom: "0xfa",                // Fantom Opera
+    FTM: "0xfa",                // Fantom Opera
+    fantomTestnet: "0xfa2",        // Fantom Testnet
+    goerli: "0x5",                 // Goerli Testnet
+    harmony: "0x63564c40",         // Harmony Mainnet
+    harmonyTestnet: "0x6357d2e0",  // Harmony Testnet
+    kovan: "0x2a",                 // Kovan Testnet
+    moonbaseAlpha: "0x507",        // Moonbase Alpha Testnet
+    moonriver: "0x505",            // Moonriver
+    mumbai: "0x13881",             // Mumbai Testnet (Polygon)
+    optimism: "0xa",               // Optimism
+    optimismKovan: "0x45",         // Optimism Kovan
+    polygon: "0x89",               // Polygon Mainnet
+    MATIC: "0x89",               // Polygon Mainnet
+    rinkeby: "0x4",                // Rinkeby Testnet
+    ropsten: "0x3",                // Ropsten Testnet
+};
+
 export const web3utils = {
-    GetNFTs: function(wallet, chainId, contract) {
+    CurrentChainId: 0x1,  // default to ethereum
+    GetNFTs: async function(wallet, chainId, contract) {
         try {
             // Switch to the specified chain
-            window.ethereum.request({
+            await window.ethereum.request({
                 method: 'wallet_switchEthereumChain',
-                params: [{ chainId: `0x${chainId.toString(16)}` }], // Chain ID must be in hexadecimal
+                params: [{ 'chainId': chainId }], // Chain ID must be in hexadecimal
             });
 
             // Get the balance of NFTs (using the ERC-721 balanceOf function signature)
-            const balanceMethod = '0x70a08231'; // balanceOf ABI signature
+	    //const balanceMethod = '0x70a08231'; // balanceOf ABI signature
+	    const balanceMethod = '0x4e1273f4'; // balanceOfBatch ABI identifier
             const balanceParams = wallet.toLowerCase().padStart(64, '0'); // 32 bytes hex format
             const balanceData = balanceMethod + balanceParams;
 
-            const balance = window.ethereum.request({
+            const balance = await window.ethereum.request({
                 method: 'eth_call',
                 params: [{
                     to: contract,
@@ -50,31 +87,44 @@ export const web3utils = {
             throw error;
         }
     },
-    ConnectWallet: function() {
+    ConnectWallet: async function(chain) {
+	chain = chain || this.CurrentChainId;
 	if (typeof window.ethereum !== 'undefined') {
 	    console.log('Ethereum wallet is detected!');
 
 	    // Use the request method to ask for the 'eth_accounts' permission
-	    window.ethereum.request({ method: 'eth_requestAccounts' })
-		.then(accounts => {
-		    if (accounts.length > 0) {
-			const account = accounts[0];
-			console.log('Connected account:', account);
-			return account;
-			// You can now use this account to interact with the Ethereum blockchain
-		    } else {
-			console.error('No accounts found.');
-			throw new Error("No accounts in your wallet");
-		    }
-		})
-		.catch(error => {
-		    console.error(`Error connecting to wallet:${error}`);
-		    throw error; // Handle any errors that occur during the request
-		});
+	    var accounts;
+	    try {
+		accounts = await window.ethereum.request({
+		    method: 'eth_requestAccounts',
+		    params: [{'chainId': chain}]
+		})	
+	    } catch(error) {
+		console.error(`Error connecting to wallet:${error}`);
+		throw error; // Handle any errors that occur during the request
+	    }
+	    
+	    if (accounts.length > 0) {
+		const account = accounts[0];
+		console.log('Connected account:', account);
+		this.CurrentChainId = chain; // if we connected successfully, store this chain
+		return account;
+		// You can now use this account to interact with the Ethereum blockchain
+	    } else {
+		console.error('No accounts found.');
+		throw new Error("No accounts in your wallet");
+	    }
 	} else {
 	    // Ethereum wallet is not available
 	    console.error('Please install an Ethereum wallet like MetaMask to use this feature.');
 	    throw new Error('Please install an Ethereum wallet like MetaMask to use this feature.');
-	}
+	}	
+    },	
+    ChainId: function(chainName) {
+	const chainId = EVMchains[chainName];
+	if (!chainId)
+	    throw new Error(`Unknown chain: ${chainName}`);
+    	return chainId;
     }
 }
+
