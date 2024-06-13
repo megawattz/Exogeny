39482e1056b2ff998e1b3cf8ac8b44a277c507e9
Walter Howard <walterhoward@gmail.com>
2024-06-12 20:49:28 -0400

    Random lifeforms background works

--- website/js/utils.mjs
+++ website/js/utils.mjs
@@ -193,3 +193,55 @@ export function sprintf() {
     return str;
 };
 
+export function fadein(element, seconds) {
+    let step = 1/(seconds * 10); // updates opacity every 100 milliseconds
+    var op = 0;
+    var timer = setInterval(function() {
+        if (op >= 0.9) {
+	    op = 1;
+	    clearInterval(timer);
+	}
+	op += step;
+	element.style.opacity = op;
+	console.log(op);
+    }, 100);
+}
+
+export function fadeout(element, seconds) {
+    let step = 1/(seconds * 10); // updates opacity every 100 milliseconds
+    var op = element.style.opacity;
+    var timer = setInterval(function() {
+        if (op <= 0.01) {
+	    op = 0;
+	    clearInterval(timer);
+	}
+	op -= step;
+	element.style.opacity = op;
+    }, 100);
+}
+
+export function randomPlanet() {
+    if (!Planets || Planets.length < 1) {
+	loadPlanets();
+	return "video/planet_852f9f7e-eb5e-4df0-be17-5f8c76da2ec4.mp4";
+    }
+    let pcount = Planets.length;
+    let index = Math.floor(Math.random() * pcount);
+    return Planets[index].planet_video;
+}
+
+export function randomCulture() {
+    let cultures = [
+	"adventurous.png",
+	"cooperative.png",
+	"egalitarian.png",
+	"hive.png",
+	"organized.png",
+	"military.png",
+	"practical.png",
+	"religious.png",
+	"selfless.png"
+    ];
+    let index = Math.floor(Math.random() * cultures.length);
+    return cultures[index];
+}
