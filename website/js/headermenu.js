73f482660e457f610eed7ddc0f774c7e869d71be
Walter Howard <walterhoward@gmail.com>
2024-06-18 16:08:21 -0400

    headermenu scales with page size

--- website/js/headermenu.js
+++ website/js/headermenu.js
@@ -23,12 +23,11 @@ function getCookie(name) {
 }
 
 document.getElementById("navheader").innerHTML = `
-      <div class="container-fluid" style="display: flex; align-items: left; width: 50vw;">
+      <div class="container-fluid">
 	<a class="navbar-brand" href="home.html">
-	  <img style="width: 50vw;" src="image/ExoPlaneteer.png">
+	  <img style="height: 6vw;" src="image/ExoPlaneteer.png">
 	</a>
-	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" 
-		aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 	  <span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarNavDropdown">
