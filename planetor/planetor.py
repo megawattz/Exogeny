d5dba1ad40868e97ef7c4438469e9fcef93e8d70
Walter Howard <walterhoward@gmail.com>
2024-06-21 00:40:00 -0400

    Keeping brightness between 0 and 1 prevents invisible rings which require a redraw

--- planetor/planetor.py
+++ planetor/planetor.py
@@ -750,7 +750,7 @@ def addrings(camera_location, ring_count, planet_size, atmosphere, options, moon
             continue
         else:
             index = index + 1
-        ring_brightness = ring_brightness + randomfloat(0, 0.4) * int(randomlist([-1, 1]))
+        ring_brightness = (ring_brightness + randomfloat(0.7, 0.9) * int(randomlist([-1, 1]))) % 1
         ring_scene += ring(color, ring_radius, hole_radius, ring_brightness, index, options, ring_template)
     return ring_scene
 
