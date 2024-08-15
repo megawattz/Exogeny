8acee383975b1b318e8918e80d01daf0fbeb3725
Walter Howard <walterhoward@gmail.com>
2024-08-15 16:47:46 -0400

    WIP

--- website/js/web3.mjs
+++ website/js/web3.mjs
@@ -64,7 +64,7 @@ export const web3utils = {
     },
     
     Authenticate: async function(exogeny_server) {
- 	let exogenyauth = utils.getCookie("exogenyauth")<;
+ 	let exogenyauth = utils.getCookie("exogenyauth");
 	if (!exogenyauth)
 	    exogenyauth = localStorage.getItem("exogenyauth");
         let response = await fetch(`${exogeny_server}/authenticate?exogenyauth=${exogenyauth}`);
