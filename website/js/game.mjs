c3bc4646581bcd51291cc1ed05ea321efddf95a6
Walter Howard <walterhoward@gmail.com>
2024-08-19 23:31:04 -0400

    WIP

---
+++ website/js/game.mjs
@@ -0,0 +1,11 @@
+// game interactions
+
+import { ethers } from "https://cdn.ethers.io/lib/ethers-5.2.esm.min.js";
+
+var Server = "https://server.exoplaneteer.com:20080";
+
+export const game = {
+    FetchEvents: async function() {
+	
+    }
+}
