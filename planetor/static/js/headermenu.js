bce2554a05c68c4da9447b363545dd86c29726df
Walter Howard <walterhoward@gmail.com>
2024-09-24 23:52:18 -0400

    WIP

--- planetor/static/js/headermenu.js
+++ planetor/static/js/headermenu.js
@@ -35,23 +35,15 @@ document.getElementById("navheader").innerHTML = `
 	<a class="navbar-brand" href="/screensaver">
 	  <img style="width: clamp(360px, 20vw, 20vw); margin-right: 0; pad-right: 0;" src="/media/images/ExoPlaneteer.png">
 	</a>
-	<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
+	<button class="navbar-toggler" type="button" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
 	  <span class="navbar-toggler-icon"></span>
 	</button>
 	<div class="collapse navbar-collapse" id="navbarNavDropdown">
 	  <ul class="navbar-nav nav-top bg-transparent">
-
-	    <li class="nav-item dropdown">
-	      <a class="nav-link dropdown-toggle" href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                 Planet Maker
-	      </a>
-	      <ul class="dropdown-menu bg-black" style="aria-labelledby="navbarDropdownMenuLink">
-		<li><a class="dropdown-item nav-link" href="/editplanet">Edit/Create Planets</a></li>
-		<li><a class="dropdown-item nav-link" href="/myplanets">Browse Planets</a></li>
-		<li><a class="dropdown-item nav-link" href="/screensaver">Screen Saver</a></li>
-		<li><a class="dropdown-item nav-link" href="/website/gallery.html">Public Website</a></li>
-	      </ul>
-	    </li>
+		<a class="nav-link" href="/editplanet">Edit</a>
+		<a class="nav-link" href="/myplanets">Browse</a>
+		<a class="nav-link" href="/screensaver">Screen Saver</a>
+		<a class="nav-link" href="/website/gallery.html">Website</a>
 	  </ul>
 	</div>
       </div>`;
